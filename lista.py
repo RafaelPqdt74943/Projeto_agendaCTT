8eb99208d84cb4a5c855bfbfedd552768b887eed
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-08-30 18:40:23 -0300

    prática do uso do for

---
+++ lista.py
@@ -0,0 +1,21 @@
+nome = "Guilherme"
+idade = 19
+
+minha_lista = ["azul", "amarelo","verde"]
+print(minha_lista)
+
+minha_lista.append("roxo")  # o metodo append adiciona elm a lista
+
+minha_lista.extend()
+
+for cor in minha_lista :
+    print(cor)
+
+
+
+
+
+
+
+
+    
