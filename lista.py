802ab9c0b690722b7f642385d744ecc4cd01a2ae
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-08-30 19:00:24 -0300

    lista do projeto

--- lista.py
+++ lista.py
@@ -1,15 +1,16 @@
 nome = "Guilherme"
 idade = 19
 
-minha_lista = ["azul", "amarelo","verde"]
+minha_lista = ["azul", "amarelo","verde", "rosa", "vermelho"]
 print(minha_lista)
 
 minha_lista.append("roxo")  # o metodo append adiciona elm a lista
 
-minha_lista.extend()
 
-for cor in minha_lista :
-    print(cor)
+print(minha_lista[0:2]) #0:2 serve para corta pedaço da lista se usar -2 ele começa a contar ao contrário
+
+#for cor in minha_lista :
+ #   print(cor)
 
 
 
