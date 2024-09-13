92c540ee072f10376c43390effaf205e70a6d356
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-09-13 19:34:06 -0300

    criando metodo para mostrar toda a agenda

--- agenda.py
+++ agenda.py
@@ -2,14 +2,26 @@
 
 AGENDA = {}
 
-#CRIANDO UM MÉTODO
-def primeiro_metodo ():
-    print("NOSSO PRIMEIRO MÉTODO")
+AGENDA ["guilherme"] = { "telefone" : "99999-7777",
+                        "email" : "gui.123@gmail.com",
+                        "endereco" : "av.123 -rj",
 
-#CHAMANDO UM MÉTODO
-primeiro_metodo()
-primeiro_metodo()
-primeiro_metodo()
-primeiro_metodo()
-primeiro_metodo()
-primeiro_metodo()
+}
+
+AGENDA ["Suzana"] = { "telefone" : "99999-8888",
+                        "email" : "SUZU.321@gmail.com",
+                        "endereco" : "av.456 -rj",
+
+}
+
+def mostrar_contatos():
+    for contato in AGENDA:
+        print("nome ", contato)
+        print("Telefone",AGENDA[contato]["telefone"])
+        print("email", AGENDA[contato]["email"])
+        print("endereço", AGENDA[contato]["endereco"])
+        print("-----------------------------------------")
+
+
+
+mostrar_contatos()
