4ed11daed22a1fa85c5f30772b02cf7ea842ae26
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-09-24 12:28:06 -0300

    criando um Menu interativo para o usuário

--- agenda.py
+++ agenda.py
@@ -47,10 +47,39 @@ def excluir_contato(contato):
     AGENDA.pop(contato) #o .pop é um método pronto do python que exclui um item e um objeto
     print("Contato excluído {} com sucesso".format(contato))
   
-  
 
-incluir_editar_contato( "Rafael", "983212344", "sgtrafael@gmail.com", "rua cap machado" )
-incluir_editar_contato( "Rafael", "99999-9999", "sgtrafael@gmail.com", "rua cap machado" )
-incluir_editar_contato("josé","9999-6523", "", None)
-excluir_contato("Miguel")
-mostrar_contatos()   
+def mostrar_menu ():
+    print("1-Mostrar todos os contatos da agenda")
+    print("2-Buscar contatos")
+    print("3-Incluir Contatos")
+    print("4-Editar Contatos")
+    print("5-Excluir Contatos")
+    print("0- Fechar Agenda")
+
+
+mostrar_menu()
+Opcao = input("escolha uma Opção: ")
+
+print(Opcao)
+
+
+if Opcao == "1" :
+    mostrar_contatos()
+elif Opcao =="2" :
+    contato = input("Digite o nome do contato")
+    buscar_contato(contato)
+elif Opcao == "3" or Opcao == "4" :
+    contato = input("Digite o nome do Contato")
+    telefone = input("Digite o telefone do Contato")
+    email = input("Digite o email do Contato")
+    endereco = input("Digite o endereço do Contato")
+    incluir_editar_contato(contato, telefone, email, endereco)
+    mostrar_contatos()
+elif Opcao == "5" : 
+    contato = input("Digite o nome do contato")
+    excluir_contato(contato)
+    mostrar_contatos()
+elif Opcao == "0" : 
+    print("Fim/fechando do programa!!!!!!!!")
+else :
+    print("DIGITE O COMANDO CORRETO!!!!!!!, OPÇÃO INVALIDA!!!")
