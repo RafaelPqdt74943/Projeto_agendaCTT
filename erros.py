988eb4b7216a01be24309a57eaefda3d5afb6327
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-09-28 13:42:25 -0300

    aula sobre tratamento de erros 2

--- erros.py
+++ erros.py
@@ -2,14 +2,17 @@
 #erros em tempo de execução
 #erros de lógica
 
+while True :
+    try:
+        a = float(input("digite um número: "))
+        b = float(input("digite o segundo número: "))
 
-
-def divisão(a, b) :
-    try :
         print(a/b)
-    except Exception as e:
-        print("Divisão invalida")
-        print(e)
-        
-        
-divisão(20,0)
+    except ZeroDivisionError as error :
+        print("não pode ser feita a divisão por zero")
+    except ValueError as error :
+        print("informe valores em números e não em letras")
+    except Exception as error :
+        print("ocorreu algum erro")
+        print(error)
+   
